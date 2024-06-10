f2e511a4eb0739bbb91a3f574035adf172294407
Andrei <91696883+AndrewBolovintsev@users.noreply.github.com>
2024-06-10 17:03:31 +0300

    Creating a repository and project

---
+++ Practice/Practice/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Андрей Боловинцев on 10.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
