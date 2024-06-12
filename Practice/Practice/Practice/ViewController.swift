7b9936e766dd2cb123ba1c989f2105ef2f2e71cf
Andrei <91696883+AndrewBolovintsev@users.noreply.github.com>
2024-06-12 15:18:50 +0300

    Passed the topic 3 Strings and Characters

---
+++ Practice/Practice/Practice/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Андрей Боловинцев on 10.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        basics()
+        basicOperators()
+        stringsAndCharacters()
+    }
+}
