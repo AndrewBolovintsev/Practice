094fa1d2566cb4a6d5a78d2005fddd6cd28bb951
Andrei <91696883+AndrewBolovintsev@users.noreply.github.com>
2024-06-14 13:02:31 +0300

    1. While loops. 2. Conditional operators

--- Practice/Practice/Control Flow.swift
+++ Practice/Practice/Control Flow.swift
@@ -7,6 +7,24 @@
 
 import Foundation
 
+class ControlFlow: Sequence, IteratorProtocol {
+    
+    var currentIndex = 0
+    let elements = [1, 2, 3, 4, 5]
+    
+    // Функция для перехода к следующему элементу
+    func next() -> Int? {
+        guard currentIndex < elements.count else { return nil }
+        defer { currentIndex += 1 }
+        return elements[currentIndex]
+    }
+    
+    // Реализуем метод makeIterator(), который возвращает экземпляр итератора
+    func makeIterator() -> Iterator {
+        return Iterator()
+    }
+}
+
 func controlFlow() {
     
     //For-in Loops
@@ -55,23 +73,202 @@ func controlFlow() {
     }
     
     //While Loops
+    /*
+        - while оценивает свое состояние в начале каждого прохода цикла.
+        - repeat—while оценивает свое состояние в конце каждого прохода цикла.
+     */
+    
+    print("Snakes")
+    let finalSquare = 25
+    var board = [Int](repeating: 0, count: finalSquare + 1)
+    print(board)
+    var square = 0
+    var diceRoll = 0
+    
+    while square < finalSquare {
+
+        // roll the dicdiceRolle
+        diceRoll += 1
+        
+        if diceRoll == 7 { diceRoll = 1 }
+        
+        // move by the rolled amount
+        square += diceRoll
+        
+        if square < board.count {
+            // if we're still on the board, move up or down for a snake or a ladder
+            square += board[square]
+        }
+    }
+    print("Game over!")
+    
+//    repeat {
+//        
+//        // move up or down for a snake or ladder
+//        square += board[square]
+//        
+//        // roll the dice
+//        diceRoll += 1
+//        
+//        if diceRoll == 7 { diceRoll = 1 }
+//        
+//        // move by the rolled amount
+//        square += diceRoll
+//        
+//    } while square < finalSquare
+//    print("Game over!")
+    
+    
+    
+    /*
+     Игра
+     Будет поле на 30 клеток, будет рандомным образом выпадать число от 1 до 6 на кубике
+     Будут специальные клетки:
+        1. Клетка 3, 12, 20 - возвращают игррока назад на 3 клетки
+        2. Клетка 15 - продвигает игрока на 5 клеток вперед
+        3. Клетка 24 - Возвращает игрока в начало
+     */
+    
+    let finalSquare1 = 30
+    var point = 0
+    var diceRoll1 = 0
+    
+    while point < finalSquare1 {
+        
+        diceRoll1 = Int.random(in: 1...6)
+        
+        point += diceRoll1
+        
+        if point == 3 || point == 12 || point == 20 {
+            point -= 3
+            print("Вовращайтесь на 3 клетки назад")
+        }
+        else if point == 15 {
+            point += 5
+            print("Продвигайтесь на 5 клеток вперед")
+        }
+        else if point == 24 {
+            point = 0
+            print("Вовращайтесь вначало")
+        }
+    }
+    print("Win!")
+    
+//    repeat {
+//        
+//        diceRoll1 = Int.random(in: 1...6)
+//        
+//        point += diceRoll1
+//        
+//        switch(point) {
+//        case 3, 12, 20:
+//            point -= 3
+//            print("Вовращайтесь на 3 клетки назад")
+//            break
+//        case 15: 
+//            point += 5
+//            print("Продвигайтесь на 5 клеток вперед")
+//            break
+//        case 24: 
+//            point = 0
+//            print("Вовращайтесь вначало")
+//            break
+//        default:
+//            break
+//        }
+//        
+//    } while point < finalSquare
+//    print("Win!")
+    
+    
+    //Conditional Statements
+    var temperatureInCelsius = 15
+    if temperatureInCelsius > 20 {
+        print("It's very cold. Consider wearing a T-shirt.")
+    }
+    else if temperatureInCelsius > 10 {
+        print("It's not that cold. Wear a sweatshirt.")
+    }
+    else {
+        print("Dress warmly.")
+    }
+    
+    let weatherAdvice = if temperatureInCelsius > 20 {
+        "It's very cold. Consider wearing a T-shirt."
+    } else if temperatureInCelsius > 10 {
+        "It's not that cold. Wear a sweatshirt."
+    } else {
+        "Dress warmly."
+    }
+    print(weatherAdvice)
+   
+    let freezeWarning: String? = if temperatureInCelsius <= 0 {
+        "It's below freezing. Watch for ice!"
+    } else {
+        nil
+    }
+    
+    let freezeWarning1 = if temperatureInCelsius <= 0 {
+        "It's below freezing. Watch for ice!"
+    } else {
+        nil as String?
+    }
+    
+    do {
+        let weatherAdvice1 = try checkTemperature(temperatureInCelsius: 30)
+        print(weatherAdvice1)
+    } 
+    catch TemperatureError.boiling {
+        print("It's boiling hot!")
+    } 
+    catch TemperatureError.cold {
+        print("It's cold!")
+    } 
+    catch {
+        print("An error occurred: \(error)")
+    }
+    
+    
+    let num: Int = 5
+    switch num {
+    case 3:
+        print(num)
+    case 5:
+        print(num)
+    default:
+        print("Number - \(num)")
+    }
+    
+    let num1 = 45
+    var str: String?
+    switch num1 {
+    case 0..<20:
+        str = "Number from 0 to 20"
+    case 20..<40:
+        str = "Number from 20 to 40"
+    case 40..<60:
+        str = "Number from 40 to 60"
+    default:
+        str = "A number greater than 60 or less than 0"
+    }
+    print(str)
+    
     
 }
 
-class ControlFlow: Sequence, IteratorProtocol {
-    
-    var currentIndex = 0
-    let elements = [1, 2, 3, 4, 5]
-    
-    // Функция для перехода к следующему элементу
-    func next() -> Int? {
-        guard currentIndex < elements.count else { return nil }
-        defer { currentIndex += 1 }
-        return elements[currentIndex]
+func checkTemperature(temperatureInCelsius: Int) throws -> String {
+    if temperatureInCelsius > 100 {
+        throw TemperatureError.boiling
+    } 
+    else if temperatureInCelsius < -25 {
+        return "Cold!"
     }
-    
-    // Реализуем метод makeIterator(), который возвращает экземпляр итератора
-    func makeIterator() -> Iterator {
-        return Iterator()
+    else {
+        return "It's a reasonable temperature."
     }
+}
+
+enum TemperatureError: Error {
+    case boiling
+    case cold
 }
