3cbdb174211e7259799bcccf32ff802c8876e041
Andrei <91696883+AndrewBolovintsev@users.noreply.github.com>
2024-06-20 09:52:00 +0300

    The topic of 9 Structures And Classes has been completed

--- Practice/Practice/Structures And Classes.swift
+++ Practice/Practice/Structures And Classes.swift
@@ -7,6 +7,70 @@
 
 import Foundation
 
+struct Resolution {
+    var height = 0
+    var width = 0
+}
+
+class VideoMode {
+    var resolution = Resolution()
+    var interlaced = false
+    var frameRate = 0.0
+    var name: String?
+}
+
+enum CompassPoint1 {
+    case north, south, east, west
+    mutating func turnNorth() {
+        self = .north
+    }
+}
+
 func structuresAndClasses() {
     
+    let someResolution = Resolution()
+    let someVideoMode = VideoMode()
+    
+    print("The width of someResolution is \(someResolution.width)")
+    print("The width of someVideoMode is \(someVideoMode.resolution.width)")
+    
+    someVideoMode.resolution.width = 1280
+    print("The width of someVideoMode is now \(someVideoMode.resolution.width)")
+    
+    let vga = Resolution(height: 480, width: 640)
+    let hd = Resolution(height: 1080, width: 1920)
+    var cinema = hd
+    cinema.width = 2048
+    
+    print(hd.height)
+    print(cinema.height)
+    
+    
+    var currentDirection = CompassPoint1.west
+    let rememberedDirection = currentDirection
+    currentDirection.turnNorth()
+
+    print(currentDirection)
+    print(rememberedDirection)
+    
+    
+    
+    let tenEighty = VideoMode()
+    tenEighty.resolution = hd
+    tenEighty.interlaced = true
+    tenEighty.name = "1080i"
+    tenEighty.frameRate = 25.0
+    
+    print(tenEighty.frameRate)
+    
+    let alsoTenEighty = tenEighty
+    alsoTenEighty.frameRate = 30.0
+    
+    print("TenEighty: \(tenEighty.frameRate)")
+    print("AlsoTenEighty: \(alsoTenEighty.frameRate)")
+    
+    
+    if tenEighty === alsoTenEighty {
+        print("tenEighty and alsoTenEighty refer to the same VideoMode instance.")
+    }
 }
