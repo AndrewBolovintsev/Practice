69a3b5d9f2ac04d5edb4b0a326d9c7b1f4a482f7
Andrei <91696883+AndrewBolovintsev@users.noreply.github.com>
2024-06-28 16:31:03 +0300

    Stopped at Overriding a Failable Initializer

--- Practice/Practice/Initialization.swift
+++ Practice/Practice/Initialization.swift
@@ -64,6 +64,94 @@ func initialization() {
     let centerRect = Rect6(center: Point6(x: 4.0, y: 4.0), size: Size6(width: 3.0, height: 3.0))
     print(centerRect)
     
+    
+    
+    let vehicle = Vehicle()
+    print("Vehicle: \(vehicle.description)")
+    
+    let bicycle = Bicycle()
+    print("Bicycle: \(bicycle.description)")
+    
+    let hoverboard = Hoverboard(color: "silver")
+    print("Hoverboard: \(hoverboard.description)")
+    
+    let namedMeat = Food(name: "Bacon")
+    print("Food(name: \"Bacon\"): name = \(namedMeat.name)")
+    
+    let mysteryMeat = Food()
+    print("Food(): name = \(mysteryMeat.name)")
+    
+    let oneMysteryItem = RecipeIngredient()
+    print(oneMysteryItem.name)
+    let oneBacon = RecipeIngredient(name: "Bacon")
+    print(oneBacon.name)
+    let twoApple = RecipeIngredient(name: "Apple", quantity: 2)
+    print("\(twoApple.name) : \(twoApple.quantity)")
+    
+    var list = [
+        ShoppingListItem(),
+        ShoppingListItem(name: "Bacon"),
+        ShoppingListItem(name: "Apple", quantity: 2),
+    ]
+    list[0].name = "Orange juice"
+    list[0].purchased = true
+    for item in list {
+        print(item.description)
+    }
+    
+    
+    
+    let someCreature = Animal(species: "Giraffe")
+    if let giraffe = someCreature {
+        print("An animal was initialized with a species of \(giraffe.species)")
+    }
+    
+    let anonymousCreature = Animal(species: "")
+    if anonymousCreature == nil {
+        print("The anonymous creature couldn't be initialized")
+    }
+    
+    
+    
+    let fahrenheitUnit = TemperatureUnit(symbol: "F")
+    if fahrenheitUnit != nil {
+        print("This is a defined temperature unit, so initialization succeeded.")
+    }
+    
+    let unknownUnit = TemperatureUnit(symbol: "X")
+    if unknownUnit == nil {
+        print("This isn't a defined temperature unit, so initialization failed.")
+    }
+    
+    
+    
+    let fahrenheitUnit1 = TemperatureUnit1(rawValue: "F")
+    if fahrenheitUnit1 != nil {
+        print("This is a defined temperature unit, so initialization succeeded.")
+    }
+
+    let unknownUnit1 = TemperatureUnit1(rawValue: "X")
+    if unknownUnit1 == nil {
+        print("This isn't a defined temperature unit, so initialization failed.")
+    }
+    
+    
+    
+    if let twoSocks = CartItem(name: "sock", quantity: 2) {
+        print("Item: \(twoSocks.name), quantity: \(twoSocks.quantity)")
+    }
+    
+    if let zeroShirts = CartItem(name: "shirt", quantity: 0) {
+        print("Item: \(zeroShirts.name), quantity: \(zeroShirts.quantity)")
+    } else {
+        print("Unable to initialize zero shirts")
+    }
+    
+    if let oneUnnamed = CartItem(name: "", quantity: 1) {
+        print("Item: \(oneUnnamed.name), quantity: \(oneUnnamed.quantity)")
+    } else {
+        print("Unable to initialize one unnamed product")
+    }
 }
 
 
@@ -192,3 +280,132 @@ struct Rect6 {
     }
 }
 
+
+
+class Vehicle {
+    
+    var numberOfWheels = 0
+    
+    var description: String {
+        return "\(numberOfWheels) wheel(s)"
+    }
+}
+class Bicycle: Vehicle {
+    
+    override init() {
+        super.init()
+        numberOfWheels = 2
+    }
+}
+
+class Hoverboard: Vehicle {
+    
+    var color: String
+    
+    init(color: String) {
+        self.color = color
+        // super.init() implicitly called here
+    }
+    
+    override var description: String {
+        return "\(super.description) in a beautiful \(color)"
+    }
+}
+
+
+
+class Food {
+    
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    convenience init() {
+        self.init(name: "[Unnamed]")
+    }
+}
+class RecipeIngredient: Food {
+    
+    var quantity: Int
+    
+    init(name: String, quantity: Int) {
+        self.quantity = quantity
+        super.init(name: name)
+    }
+    
+    override convenience init(name: String) {
+        self.init(name: name, quantity: 1)
+    }
+}
+class ShoppingListItem: RecipeIngredient {
+    
+    var purchased = false
+    
+    var description: String {
+        var output = "\(quantity) - \(name)"
+        output += purchased ? " ✔︎" : " x"
+        return output
+    }
+}
+
+
+
+struct Animal {
+    
+    let species: String
+    
+    init?(species: String) {
+        if species.isEmpty { return nil }
+        self.species = species
+    }
+}
+
+
+
+enum TemperatureUnit {
+    case kelvin, celsius, fahrenheit
+    init?(symbol: Character) {
+        switch symbol {
+        case "K":
+            self = .kelvin
+        case "C":
+            self = .celsius
+        case "F":
+            self = .fahrenheit
+        default:
+            return nil
+        }
+    }
+}
+
+enum TemperatureUnit1: Character {
+    case kelvin = "K", celsius = "C", fahrenheit = "F"
+}
+
+
+
+class Product {
+    
+    let name: String
+    
+    init?(name: String) {
+        
+        if name.isEmpty { return nil }
+        
+        self.name = name
+    }
+}
+class CartItem: Product {
+    
+    let quantity: Int
+    
+    init?(name: String, quantity: Int) {
+        
+        if quantity < 1 { return nil }
+        
+        self.quantity = quantity
+        super.init(name: name)
+    }
+}
