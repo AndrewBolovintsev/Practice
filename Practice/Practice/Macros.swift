13a469dd02692d6d29b6fdced5561541fb079626
Andrei <91696883+AndrewBolovintsev@users.noreply.github.com>
2024-07-08 16:27:50 +0300

    Create file Macros

---
+++ Practice/Practice/Macros.swift
@@ -0,0 +1,17 @@
+//
+//  Macros.swift
+//  Practice
+//
+//  Created by Андрей Боловинцев on 8.07.24.
+//
+
+import Foundation
+
+func macros() {
+    myFunction()
+}
+
+func myFunction() {
+    print("Currently running \(#function)")
+    #warning("Something's wrong")
+}
