4413bedd1e1c6564a2d0ff1a0627f0d76b214e0a
Andrei <91696883+AndrewBolovintsev@users.noreply.github.com>
2024-07-11 12:41:38 +0300

    The topic of Type Casting has been passed

--- Practice/Practice/TypeCasting.swift
+++ Practice/Practice/TypeCasting.swift
@@ -9,4 +9,135 @@ import Foundation
 
 func typeCasting() {
     
+    let library = [
+        Movie(name: "Casablanca", director: "Michael Curtiz"),
+        Song(name: "Blue Suede Shoes", artist: "Elvis Presley"),
+        Movie(name: "Citizen Kane", director: "Orson Welles"),
+        Song(name: "The One And Only", artist: "Chesney Hawkes"),
+        Song(name: "Never Gonna Give You Up", artist: "Rick Astley")
+    ]
+    
+    var movieCount = 0
+    var songCount = 0
+
+    for item in library {
+        if item is Movie {
+            movieCount += 1
+        } else if item is Song {
+            songCount += 1
+        }
+    }
+
+    print("Media library contains \(movieCount) movies and \(songCount) songs")
+    
+    for item in library 
+    {
+        if let movie = item as? Movie
+        {
+            print("Movie: \(movie.name), dir. \(movie.director)")
+        } 
+        else if let song = item as? Song
+        {
+            print("Song: \(song.name), by \(song.artist)")
+        }
+    }
+    
+    
+    var things: [Any] = []
+
+    things.append(5)
+    things.append(0.4)
+    things.append(345)
+    things.append(3.14159)
+    things.append("hello")
+    things.append((3.0, 5.0))
+    things.append(Movie(name: "Ghostbusters", director: "Ivan Reitman"))
+    things.append({ (name: String) -> String in "Hello, \(name)" })
+    
+    for thing in things {
+        switch thing {
+        case 5 as Int:
+            print("5 as an Int")
+        case 0.4 as Double:
+            print("0.4 as a Double")
+        case let someInt as Int:
+            print("an integer value of \(someInt)")
+        case let someDouble as Double where someDouble > 0:
+            print("a positive double value of \(someDouble)")
+        case is Double:
+            print("some other double value that I don't want to print")
+        case let someString as String:
+            print("a string value of \"\(someString)\"")
+        case let (x, y) as (Double, Double):
+            print("an (x, y) point at \(x), \(y)")
+        case let movie as Movie:
+            print("a movie called \(movie.name), dir. \(movie.director)")
+        case let stringConverter as (String) -> String:
+            print(stringConverter("Michael"))
+        default:
+            print("something else")
+        }
+    }
+    
+    let optionalNumber: Int? = 3
+    things.append(optionalNumber)
+    things.append(optionalNumber as Any)
+
+    
+    var animals: [AnyObject] = []
+
+    animals.append(Dogs(name: "Rex"))
+    animals.append(Cat(name: "Whiskers"))
+    animals.append(Dogs(name: "Buddy"))
+    animals.append(Cat(name: "Shadow"))
+    
+    for animal in animals 
+    {
+        if let dog = animal as? Dogs
+        {
+            print("Dog: \(dog.name)")
+        } 
+        else if let cat = animal as? Cat
+        {
+            print("Cat: \(cat.name)")
+        }
+    }
+}
+
+class MediaItem {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+final class Movie: MediaItem {
+    var director: String
+    
+    init(name: String, director: String) {
+        self.director = director
+        super.init(name: name)
+    }
+}
+final class Song: MediaItem {
+    var artist: String
+    
+    init(name: String, artist: String) {
+        self.artist = artist
+        super.init(name: name)
+    }
+}
+
+
+final class Dogs {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+final class Cat {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
 }
